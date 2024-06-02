4a412674e31f41d639c38a5f5545a7b0d782702a
Jakub <dsw52682@student.dsw.edu.pl>
2024-06-02 11:00:29 +0200

    Dodano --logs

--- skrypt3.sh
+++ skrypt3.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
-    date
+if [ "$1" == "--logs" ]; then
+    count=100
+    if [ ! -z "$2" ]; then
+        count=$2
+    fi
+    for ((i=1; i<=count; i++))
+    do
+        filename="log$i.txt"
+        echo "Filename: $filename" > $filename
+        echo "Script name: $0" >> $filename
+        echo "Date: $(date)" >> $filename
+    done
 fi
